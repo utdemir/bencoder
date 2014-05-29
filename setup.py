5ae10efd0782d644416aecf921a3541cd7bddb68
Utku Demir <utdemir@gmail.com>
2014-05-29 11:27:56 +0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup
+
+setup(name='bencoder',
+      version='0.1.0',
+      description='A bencode decoder/encoder library.',
+      url='https://github.com/utdemir/bencoder',
+      author='Utku Demir',
+      author_email='utdemir@gmail.com',
+      py_modules=['bencoder'],
+      classifiers=[
+          'Environment :: Other Environment',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: GNU General Public License (GPL)',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 3',
+          'Topic :: Software Development :: Libraries :: Python Modules'
+          ],
+      )
