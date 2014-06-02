e5ecda59ba310047abda2e7189baef399be953cf
Utku Demir <utdemir@gmail.com>
2014-06-02 19:19:30 +0300

    Bump version number

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='bencoder',
-      version='0.1.0',
+      version='0.2.0',
       description='A simple bencode decoder/encoder library in pure Python.',
       url='https://github.com/utdemir/bencoder',
       author='Utku Demir',
